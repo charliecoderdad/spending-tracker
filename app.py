47a082808a7dff266148c9ccad29afff6b5afc7c
Charlie Hart <charliecoderdad@gmail.com>
2020-06-14 09:49:02 -0400

    working site before refactor into easier to use package

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, url_for
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+import db_actions
+
+database = "sqlite:///database/spending.db"
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database
+app.config['SECRET_KEY'] = "WooWeeWhateverKey"
+db = SQLAlchemy(app)
+
+
 
 @app.route("/")
 def home():
@@ -9,7 +18,12 @@ def home():
     
 @app.route("/configure")
 def configure():
-    return render_template('configure.html')
+	users = [ "Charlie", "Abby", "Caylee" ]
+	return render_template('configure.html', users=users)
+	
+@app.route("/data")
+def showData():
+    return render_template('data.html')
 
 if __name__=="__main__":
 	app.run(debug=True)
