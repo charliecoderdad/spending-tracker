114277d6afea57fe8610a10cd9455e574081ffe0
Charlie Hart <charliecoderdad@gmail.com>
2020-06-18 13:24:00 -0400

    Flash messages now working

--- tracker_app/routes.py
+++ tracker_app/routes.py
@@ -14,15 +14,18 @@ def configure():
 		user = User(username=newUserForm.username.data)
 		db.session.add(user)
 		db.session.commit()
-		flash('User has been added', 'success')
+		flash(f"User '{user.username}' has been successfully added", "success")
 		return redirect(url_for('configure'))
 	return render_template('configure.html', users=User.query.all(), newUserForm=newUserForm)
 	
 @app.route("/deleteUser/<userid>", methods=["GET","POST"])
 def deleteUser(userid):
 	print(f"Charlie debug: User to delete {userid}")
+	deletedUser = User.query.filter(User.id == userid).first().username
+	print(f"Charlie test: {deletedUser}")
 	User.query.filter(User.id == userid).delete()
 	db.session.commit()
+	flash(f"User '{deletedUser}' has been successfully removed", "success")
 	return redirect(url_for('configure'))
 	
 @app.route("/data")
