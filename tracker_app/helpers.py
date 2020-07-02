8520c8bb3b530fdcb9a46d17c127d4870838edc8
Charlie Hart <charliecoderdad@gmail.com>
2020-07-02 14:32:14 -0400

    Make table columns sortable!

--- tracker_app/helpers.py
+++ tracker_app/helpers.py
@@ -1,7 +1,15 @@
 def getTableHeadTags(headersList):
-	tableHead = "<table border=1 class='table table-sm table-hover'>"
+	tableHead = "<table border=1 class='table table-sm table-hover js-sort-table'>"
 	tableHead += "<thead class='thead-dark'><tr>"
 	for item in headersList:
-		tableHead += "<th scope='col'>" + item + "</th>"
+		# If item is a monetary unit we need to add js-sort-number class to the th element
+		if (item == "Total" or item == "Min. Spending" or item == "Discretionary" or item == "Amount" or item == "Percent"):
+			tableHead += "<th scope='col' class='js-sort-number'>" + item + "</th>"
+		elif (item == "Date"):
+			tableHead += "<th scope='col' class='js-sort-date'>" + item + "</th>"
+		elif (item == "Month"):
+			tableHead += "<th scope='col' class='js-sort-month'>" + item + "</th>"
+		else:
+			tableHead += "<th scope='col'>" + item + "</th>"
 	tableHead += "</tr></thead>"
 	return tableHead;
